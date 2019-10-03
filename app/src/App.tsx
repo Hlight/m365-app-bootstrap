f54479fe75ab5b2d3ef66183f142af5bf187670a
Aaron <aaronostrowsky@gmail.com>
2019-10-02 17:36:30 -0700

    add bootstrap project

---
+++ app/src/App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import "office-ui-fabric-core/dist/css/fabric.min.css";
+import { getClassNames } from "./App.styles";
+import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
+
+import { Fabric, Link } from "office-ui-fabric-react";
+import { ShellHeader } from "./components/ShellHeader/ShellHeader";
+import { Nav } from "./components/Nav/Nav";
+
+import { DefaultContent } from "./components/DefaultContent/DefaultContent";
+
+// Icons must be initialized in order to load their styles
+initializeIcons(/* optional base url */);
+
+
+
+class App extends React.Component {
+  render() {
+    const {
+      root,
+      header,
+      mainWrapper,
+      content,
+      pivotsAndSearch,
+      pivotContainer,
+      footer,
+      searchBar
+    } = getClassNames();
+    return (
+      <Fabric>
+        <div className={root}>
+          <ShellHeader />
+          <section className={mainWrapper}>
+            <main className={content} data-is-scrollable={true}>
+              <DefaultContent />
+            </main>
+            <Nav isNavCollapsed={true} />
+          </section>
+          <footer className={footer} />
+        </div>
+      </Fabric>
+    );
+  }
+}
+
+export default App;
